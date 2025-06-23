803f6c5d1920f3111e735979968d8a9359e5eabf
Utkarsh Jha <64651522+UtkarshJha55@users.noreply.github.com>
2025-06-23 22:40:34 +0530

    Testbench file for FIFO

---
+++ tb.sv
@@ -0,0 +1,28 @@
+// Testbench for FIFO verification
+// It creates 10 transactions for verifying the design 
+
+module tb;
+    
+  fifo_if fif();
+  FIFO dut (fif.clock, fif.rst, fif.wr, fif.rd, fif.data_in, fif.data_out, fif.empty, fif.full);
+    
+  initial begin
+    fif.clock <= 0;
+  end
+    
+  always #10 fif.clock <= ~fif.clock;
+    
+  environment env;
+    
+  initial begin
+    env = new(fif);
+    env.gen.count = 10;
+    env.run();
+  end
+    
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+   
+endmodule
